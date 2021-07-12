b637195ef8805facaea023553c6a1fca4f075833
Ananda Farhan <nandafarhan@gmail.com>
2021-07-12 21:02:42 +0700

    Code overhaul
    - Separate header and footer
    - New model, better OOP

--- AddProduct.php
+++ AddProduct.php
@@ -1,197 +1,146 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "root";
-// $dbname = "latihandb";
-
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-include('database.php');
-$db = new database();
+include('./model/Product.php');
+include('./model/Category.php');
+include('./model/Supplier.php');
+$product = new Product();
+$category = new Category();
+$supplier = new Supplier();
+include('template-Header.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css" rel="stylesheet">
-  <title>Do do do</title>
-</head>
-
-<body>
-  <!-- Nav Start -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Daftar Produk</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/AddProduct.php">Tambah Produk</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto">
-          <li>
-            <form class="d-flex">
-              <button class="btn btn-outline-success btn-sm" type="button">Login</button>
-            </form>
-          </li>
-        </ul>
+<div class="container mt-3">
+  <h2>Tambah Produk</h2>
+  <div class="container mt-4 px-5">
+    <form method="post" action="./model/Product.php?func=createproduct">
+      <div class="mb-3">
+        <label for="ProductName" class="form-label">Product Name</label>
+        <input type="text" class="form-control" id="ProductName" name="productname" required>
       </div>
-    </div>
-  </nav>
-  <!-- Nav End -->
-  <div class="container mt-3">
-    <h2>Tambah Produk</h2>
-    <div class="container mt-4 px-5">
-      <form method="post" action="function.php?func=addproduct">
-        <div class="mb-3">
-          <label for="ProductName" class="form-label">Product Name</label>
-          <input type="text" class="form-control" id="ProductName" name="productname" required>
-        </div>
-        <div class="mb-3">
-          <label for="sku" class="form-label">SKU</label>
-          <input type="text" class="form-control" id="sku" name="sku" required>
-        </div>
-        <div class="mb-3">
-          <label for="stock" class="form-label">Stock</label>
-          <input type="number" class="form-control" id="stock" name="stock" required>
-        </div>
-        <div class="mb-3">
-          <label for="category" class="form-label">Category</label>
-          <select class="form-select" id="category" aria-label="Category" name="category">
-            <?php
-            if ($db->getAllCategory()->num_rows > 0) {
-              foreach ($db->getAllCategory() as $row) {
-            ?>
-                <option value="<?= $row['CategoryId']; ?>"><?= $row['CategoryName']; ?></option>
-            <?php
-              }
+      <div class="mb-3">
+        <label for="sku" class="form-label">SKU</label>
+        <input type="text" class="form-control" id="sku" name="sku" required>
+      </div>
+      <div class="mb-3">
+        <label for="stock" class="form-label">Stock</label>
+        <input type="number" class="form-control" id="stock" name="stock" required>
+      </div>
+      <div class="mb-3">
+        <label for="category" class="form-label">Category</label>
+        <select class="form-select" id="category" aria-label="Category" name="category">
+          <?php
+          if ($category->getAllCategory()->num_rows > 0) {
+            foreach ($category->getAllCategory() as $row) {
+          ?>
+              <option value="<?= $row['CategoryId']; ?>"><?= $row['CategoryName']; ?></option>
+          <?php
             }
-            ?>
-          </select>
-          <div id="emailHelp" class="form-text">
-            <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#categoryModal">
-              Add Category
-            </button>
+          }
+          ?>
+        </select>
+        <div id="emailHelp" class="form-text">
+          <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#categoryModal">
+            Add Category
+          </button>
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="supplier" class="form-label">Supplier</label>
+        <select class="form-select" id="supplier" aria-label="Supplier" name="supplier">
+          <?php
+          if ($supplier->getAllSupplier()->num_rows > 0) {
+            foreach ($supplier->getAllSupplier() as $row) {
+          ?>
+              <option value="<?= $row['SupplierId']; ?>"><?= $row['CompanyName']; ?></option>
+          <?php
+            }
+          }
+          ?>
+        </select>
+        <div id="emailHelp" class="form-text">
+          <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#supplierModal">
+            Add Supplier
+          </button>
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="cost" class="form-label">Cost Price</label>
+        <input type="number" class="form-control" id="cost" name="cost" required>
+      </div>
+      <div class="mb-3">
+        <label for="sale" class="form-label">Sale Price</label>
+        <input type="number" class="form-control" id="sale" name="sale" required>
+      </div>
+      <button type="submit" class="btn btn-primary" name="addproduct">Submit</button>
+    </form>
+  </div>
+</div>
+
+<!-- Category Modal Start -->
+<div class="modal fade" id="categoryModal" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Add Category</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form method="POST" action="./model/Category.php?func=createcategory">
+        <div class="modal-body">
+          <div class="mb-3">
+            <label for="addCategory" class="col-form-label">Category:</label>
+            <input type="text" class="form-control" id="addCategory" name="newcat" required>
           </div>
         </div>
-        <div class="mb-3">
-          <label for="supplier" class="form-label">Supplier</label>
-          <select class="form-select" id="supplier" aria-label="Supplier" name="supplier">
-            <?php
-            if ($db->getAllSupplier()->num_rows > 0) {
-              foreach ($db->getAllSupplier() as $row) {
-            ?>
-                <option value="<?= $row['SupplierId']; ?>"><?= $row['CompanyName']; ?></option>
-            <?php
-              }
-            }
-            ?>
-          </select>
-          <div id="emailHelp" class="form-text">
-            <button type="button" class="btn btn-sm btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#supplierModal">
-              Add Supplier
-            </button>
-          </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary" name="addcategory">Save</button>
         </div>
-        <div class="mb-3">
-          <label for="cost" class="form-label">Cost Price</label>
-          <input type="number" class="form-control" id="cost" name="cost" required>
-        </div>
-        <div class="mb-3">
-          <label for="sale" class="form-label">Sale Price</label>
-          <input type="number" class="form-control" id="sale" name="sale" required>
-        </div>
-        <button type="submit" class="btn btn-primary" name="addproduct">Submit</button>
       </form>
     </div>
   </div>
+</div>
+<!-- Category Modal End -->
 
-  <!-- Category Modal Start -->
-  <div class="modal fade" id="categoryModal" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Add Category</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <form method="POST" action="function.php?func=addcategory">
-          <div class="modal-body">
-            <div class="mb-3">
-              <label for="addCategory" class="col-form-label">Category:</label>
-              <input type="text" class="form-control" id="addCategory" name="newcat" required>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary" name="addcategory">Save</button>
-          </div>
-        </form>
+<!-- Supplier Modal Start -->
+<div class="modal fade" id="supplierModal" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Add Supplier</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
+      <form method="post" action="./model/Supplier.php?func=createsupplier">
+        <div class="modal-body">
+          <div class="mb-3">
+            <label for="compName" class="col-form-label">Company Name:</label>
+            <input type="text" class="form-control" id="compName" name="newcomp" required>
+          </div>
+          <div class="mb-3">
+            <label for="conName" class="col-form-label">Contact Name:</label>
+            <input type="text" class="form-control" id="contName" name="newcont" required>
+          </div>
+          <div class="mb-3">
+            <label for="Addr" class="col-form-label">Address:</label>
+            <input type="text" class="form-control" id="Addr" name="newaddr" required>
+          </div>
+          <div class="mb-3">
+            <label for="email" class="col-form-label">Email:</label>
+            <input type="email" class="form-control" id="email" name="newmail" required>
+          </div>
+          <div class="mb-3">
+            <label for="phone" class="col-form-label">Phone:</label>
+            <input type="number" class="form-control" maxlength="14" id="phone" name="newphone" required>
+          </div>
+          <div class="mb-3">
+            <label for="webs" class="col-form-label">Website:</label>
+            <input type="text" class="form-control" id="webs" name="newsite" required>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary" name="addsupplier">Save</button>
+        </div>
+      </form>
     </div>
   </div>
-  <!-- Category Modal End -->
-
-  <!-- Supplier Modal Start -->
-  <div class="modal fade" id="supplierModal" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Add Supplier</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <form method="post" action="function.php?func=addsupplier">
-          <div class="modal-body">
-            <div class="mb-3">
-              <label for="compName" class="col-form-label">Company Name:</label>
-              <input type="text" class="form-control" id="compName" name="newcomp" required>
-            </div>
-            <div class="mb-3">
-              <label for="conName" class="col-form-label">Contact Name:</label>
-              <input type="text" class="form-control" id="contName" name="newcont" required>
-            </div>
-            <div class="mb-3">
-              <label for="Addr" class="col-form-label">Address:</label>
-              <input type="text" class="form-control" id="Addr" name="newaddr" required>
-            </div>
-            <div class="mb-3">
-              <label for="email" class="col-form-label">Email:</label>
-              <input type="email" class="form-control" id="email" name="newmail" required>
-            </div>
-            <div class="mb-3">
-              <label for="phone" class="col-form-label">Phone:</label>
-              <input type="number" class="form-control" maxlength="14" id="phone" name="newphone" required>
-            </div>
-            <div class="mb-3">
-              <label for="webs" class="col-form-label">Website:</label>
-              <input type="text" class="form-control" id="webs" name="newsite" required>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary" name="addsupplier">Save</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-  <!-- Supplier Modal End -->
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-
-</html>
+</div>
+<!-- Supplier Modal End -->
--- index.php
+++ index.php
@@ -1,123 +1,105 @@
 <?php
-include('database.php');
-$db = new database();
+include('./model/connect.php');
+include('./model/Product.php');
+$product = new Product();
+include('template-Header.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet">
-  <link href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css" rel="stylesheet">
-  <title>Do do do</title>
-</head>
-
-<body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Daftar Produk</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/AddProduct.php">Tambah Produk</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto">
-          <li>
-            <form class="d-flex">
-              <button class="btn btn-outline-success btn-sm" type="button">Login</button>
-            </form>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <div class="container mt-5">
-    <table id="dataTable" class="table table-bordered table-hover">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Product Name</th>
-          <th scope="col">SKU</th>
-          <th scope="col">Stock Count</th>
-          <th scope="col">Category</th>
-          <th scope="col">Supplier</th>
-          <th scope="col">Cost Price</th>
-          <th scope="col">Sale Price</th>
-          <th scope="col">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $x = 0;
-        if ($db->getallproduct()->num_rows > 0) {
-          foreach ($db->getallproduct() as $row) {
-        ?>
-            <tr>
-              <th><?php echo $x += 1 ?></th>
-              <td><?php echo $row['ProductName']; ?></td>
-              <td><?php echo $row['SKU']; ?></td>
-              <td><?php echo $row['Stock']; ?></td>
-              <td><?php echo $row['CategoryName']; ?></td>
-              <td><?php echo $row['CompanyName']; ?></td>
-              <td><?php echo $row['CostPrice']; ?></td>
-              <td><?php echo $row['SalePrice']; ?></td>
-              <td>
-                <div class="btn-group me-2 d-flex justify-content-center" role="group">
-                  <button type="button" class="btn btn-sm btn-primary"><i class="bi-eye-fill"></i></button>
-                  <button type="button" class="btn btn-sm btn-warning edit" data-id="<?= $row['SKU']; ?>" data-product="<?= $row['ProductName']; ?>" data-cost="<?= $row['CostPrice']; ?>" data-sale="<?= $row['SalePrice']; ?>"><i class="bi-pencil-square"></i></button>
-                  <button type="button" class="btn btn-sm btn-danger del" data-id="<?= $row['SKU']; ?>" data-product="<?= $row['ProductName']; ?>"><i class="bi-trash-fill"></i></button>
-                </div>
-              </td>
-            </tr>
-        <?php
-          }
+<div class="container mt-5">
+  <table id="dataTable" class="table table-bordered table-hover">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Product Name</th>
+        <th scope="col">SKU</th>
+        <th scope="col">Stock Count</th>
+        <th scope="col">Category</th>
+        <th scope="col">Supplier</th>
+        <th scope="col">Cost Price</th>
+        <th scope="col">Sale Price</th>
+        <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      $x = 0;
+      if ($product->getAllProduct()->num_rows > 0) {
+        foreach ($product->getAllProduct() as $row) {
+      ?>
+          <tr>
+            <th><?php echo $x += 1 ?></th>
+            <td><?php echo $row['ProductName']; ?></td>
+            <td><?php echo $row['SKU']; ?></td>
+            <td><?php echo $row['Stock']; ?></td>
+            <td><?php echo $row['CategoryName']; ?></td>
+            <td><?php echo $row['CompanyName']; ?></td>
+            <td><?php echo $row['CostPrice']; ?></td>
+            <td><?php echo $row['SalePrice']; ?></td>
+            <td>
+              <div class="btn-group me-2 d-flex justify-content-center" role="group">
+                <button type="button" class="btn btn-sm btn-primary"><i class="bi-eye-fill"></i></button>
+                <button type="button" class="btn btn-sm btn-warning edit" data-id="<?= $row['SKU']; ?>" data-product="<?= $row['ProductName']; ?>" data-cost="<?= $row['CostPrice']; ?>" data-sale="<?= $row['SalePrice']; ?>"><i class="bi-pencil-square"></i></button>
+                <button type="button" class="btn btn-sm btn-danger del" data-id="<?= $row['SKU']; ?>" data-product="<?= $row['ProductName']; ?>"><i class="bi-trash-fill"></i></button>
+              </div>
+            </td>
+          </tr>
+      <?php
         }
-        ?>
-      </tbody>
-    </table>
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
+
+<!-- Edit Product Modal Start -->
+<div class="modal fade" id="modalEdit" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="title"></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form method="POST" action="./model/Product.php?func=editproduct">
+        <div class="modal-body">
+          <div class="mb-3">
+            <label for="cost" class="form-label">Cost Price</label>
+            <input type="number" step="50000" class="form-control" id="cost" name="cost" required>
+          </div>
+          <div class="mb-3">
+            <label for="sale" class="form-label">Sale Price</label>
+            <input type="number" step="50000" class="form-control" id="sale" name="sale" required>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+          <button type="submit" class="btn btn-primary" id="edid" name="editproduct">Save</button>
+        </div>
+      </form>
+    </div>
   </div>
+</div>
+<!-- Edit Product Modal End -->
 
-  <?php include('modal.php'); ?>
+<!-- Delete Product Modal Start -->
+<div class="modal fade" id="modalDelete" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Delete Confirmation</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <form method="POST" action="./model/Product.php?func=destroyproduct">
+        <div class="modal-body">
+          <p>Are you sure want to delete <strong id="product"></strong> ?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cancel</button>
+          <button type="submit" class="btn btn-danger" id="delid" name="deleteproduct">Delete!</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<!-- Delete Product Modal End -->
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-  <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap5.min.js"></script>
-  <script>
-    $(document).ready(function() {
-      $('#dataTable').DataTable();
-    });
-
-    $('.edit').on('click', function(e) {
-      var product = $(this).data('product');
-      var cost = $(this).data('cost');
-      var sale = $(this).data('sale');
-      var id = $(this).data('id');
-      $('#modalEdit').modal('show');
-      $('#title').html("Edit " + product);
-      $('#cost').val(cost);
-      $('#sale').val(sale);
-      $('#edid').attr('value', id);
-    });
-
-    $('.del').on('click', function(e) {
-      var product = $(this).data('product');
-      var id = $(this).data('id');
-      $('#modalDelete').modal('show');
-      $('#product').html(product);
-      $('#delid').attr('value', id);
-    });
-  </script>
-</body>
-
-</html>
+<?php include('template-Footer.php'); ?>
