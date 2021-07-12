79b567389b6e7218e2a7a0796b9161f5d2872a28
Ananda Farhan <nandafarhan@gmail.com>
2021-07-12 22:43:22 +0700

    Nav minor changes

--- template-Header.php
+++ template-Header.php
@@ -8,11 +8,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/css/bootstrap.min.css" rel="stylesheet">
+
     <title>Do do do</title>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,16 +23,13 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Daftar Produk</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="AddProduct.php">Tambah Produk</a>
+                        <a class="nav-link active" aria-current="page" href="/">Product List</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav ms-auto">
                     <li>
                         <form class="d-flex">
-                            <button class="btn btn-outline-success btn-sm" type="button">Login</button>
+                            <button class="btn btn-outline-light btn-sm" type="button">Login</button>
                         </form>
                     </li>
                 </ul>
